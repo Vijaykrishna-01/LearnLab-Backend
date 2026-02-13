1cdbde0a199db14c9a295050cd167b633316f12c
Vijaykrishna <vk856549@gmail.com>
2026-02-13 19:55:37 +0530

    improved middleware

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const cors = require("cors");
 const express = require("express");
 const { connectDB } = require("./config/database.js");
 const cookieParser = require("cookie-parser");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const userRoute = require("./routes/userRoute.js");
 const courseRoute = require("./routes/courseRoute.js");
 const reviewRoute = require("./routes/reviewRoute.js");
 const cartRoute = require("./routes/cartRoute.js");
-const wishlistRoute = require("./routes/wishlistRoute.js")
+const wishlistRoute = require("./routes/wishlistRoute.js");
 const categoryRoute = require("./routes/categoryRoute.js");
 const AssignmentRoute = require("./routes/assignmentRoute.js");
 const StudyPlanRoute = require("./routes/studyPlanRoute.js");
-const stripeRoute = require('./routes/stripeRoute.js');
+const stripeRoute = require("./routes/stripeRoute.js");
 const authRoute = require("./routes/authRoute.js");
 
 const app = express();
@@ -23,25 +23,29 @@ const port = process.env.PORT || 3000; // Ensure default value if PORT is not se
 app.post(
   "/api/payment/webhook",
   express.raw({ type: "application/json" }),
-  require("./routes/stripeRoute.js")
+  require("./routes/stripeRoute.js"),
 );
 
 // Connect to Database
 connectDB();
 
 // Basic route for testing connection
-app.get('/', async (req, res) => { res.send('Connected'); });
+app.get("/", async (req, res) => {
+  res.send("Connected");
+});
 
 // Middleware
 app.use(
   cors({
+    origin: true, 
     credentials: true,
-    origin: true,
-  })
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
 );
 
 // ✅ Handle preflight OPTIONS requests for all routes
-app.options('*', cors());
+app.options("*", cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
@@ -52,12 +56,12 @@ app.use("/user", userRoute);
 app.use("/courses", courseRoute);
 app.use("/rating", reviewRoute);
 app.use("/cart", cartRoute);
-app.use("/wishlist", wishlistRoute)
+app.use("/wishlist", wishlistRoute);
 app.use("/category", categoryRoute);
 app.use("/assignment", AssignmentRoute);
 app.use("/studyPlan", StudyPlanRoute);
 app.use("/auth", authRoute);
-app.use('/payment', stripeRoute);
+app.use("/payment", stripeRoute);
 
 // Listen on specified port
 app.listen(port, () => {
